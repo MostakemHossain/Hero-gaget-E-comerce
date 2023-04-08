40be7f3bf619de01a1d8e87cea9ca4c31afddbde
MostakemHossain <mostakememon123@gmail.com>
2023-04-09 05:00:55 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,32 @@ import './index.css'
 import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import About from './components/About'
+import Home from './components/Home'
+import ErrorPage from './components/ErrorPage'
+import Shop from './components/Shop/Shop'
 
 const router= createBrowserRouter([
     {
         path:"/",
-        element:<App></App>
+        element:<App></App>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"/about",
+                element:<About></About>
+            },
+            {
+                path:'/shop',
+                element:<Shop></Shop>
+            }
+        ]
+
     },
-    {
-        path:"/about",
-        element:<About></About>
-    }
+    
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}></RouterProvider>)
