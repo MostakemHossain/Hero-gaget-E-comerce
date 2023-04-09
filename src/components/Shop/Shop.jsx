a820b87792f94e919833f9b21d7d7a2f50eed25e
MostakemHossain <mostakememon123@gmail.com>
2023-04-09 06:01:10 +0600

    local-storage set

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { addToDb } from '../../utilities/FakeDB';
+
+
+
+import ProductCard from '../Cards/ProductCard';
 
 const Shop = () => {
+    const productData= useLoaderData();
+
+    const handleAddToCart=(id)=>{
+       addToDb(id)
+    }
+    
     return (
-        <div>
-            <h1>Shop</h1>
+        <div className='product-container' >
+        {
+            productData.map(product=> <ProductCard key={product.id} product={product} handleAddToCart={handleAddToCart}></ProductCard>)
+        }
+            
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,8 @@ const router= createBrowserRouter([
             },
             {
                 path:'/shop',
-                element:<Shop></Shop>
+                element:<Shop></Shop>,
+                loader:()=> fetch('products.json')
             }
         ]
 
